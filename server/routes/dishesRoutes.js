6624426627177bc35961d4e119f1bd1908694b6b
Yaroslav <usern1861@gmail.com>
2025-05-04 23:27:37 +0300

    Umami House update 0.1.3
    Some updates

---
+++ server/routes/dishesRoutes.js
@@ -0,0 +1,21 @@
+import express from "express"
+import {
+  handleAddDish,
+  handleUpdateDish,
+  handleDeleteDish,
+  handleAddImageByDish,
+  handleDeleteImageByDish,
+  handleGetDishById,
+} from "../controllers/dishes/dishesAdminControllers"
+
+const router = express.Router()
+
+router.post("/dishes/add", handleAddDish)
+router.post("/dishes/update", handleUpdateDish)
+router.post("/dishes/delete", handleDeleteDish)
+router.post("/dishes/add-image", handleAddImageByDish)
+router.post("/dishes/delete-image", handleDeleteImageByDish)
+
+router.get("/dishes/:id", handleGetDishById)
+
+export default router
---
+++ server/server.ts
@@ -0,0 +1,13 @@
+import express from "express"
+import cors from "cors"
+import dishesRouter from "./routes/dishesRoutes.js"
+const app = express()
+
+console.log("DAS")
+app.use(express.json())
+app.use(cors())
+app.use("/", dishesRouter)
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
