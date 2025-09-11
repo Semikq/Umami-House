26e4832a76e30fdc09ae1280d22067b6aeee1cc6
Yaroslav <usern1861@gmail.com>
2025-09-11 17:04:17 +0300

    Umami-House 0.4.3

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import path from "path";
-import { fileURLToPath } from "url";
 import dishesRouter from "./routes/dishesRoutes.js";
 import favoritesRoutes from "./routes/favoritesRoutes.js";
 import ordersRoutes from "./routes/ordersRoutes.js";
@@ -11,11 +9,6 @@ import restaurantsRoutes from "./routes/restaurantsRoutes.js";
 import saleRoutes from "./routes/saleRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 const app = express();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-console.log(process.env.CLIENT_URL)
 
 app.use(express.json());
 app.use(cookieParser());
@@ -32,13 +25,6 @@ app.use("/partners", partnersRoutes);
 app.use("/restaurants", restaurantsRoutes);
 app.use("/sales", saleRoutes);
 app.use("/users", userRoutes);
-
-const clientBuildPath = path.join(__dirname, "../client/build");
-app.use(express.static(clientBuildPath));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(clientBuildPath, "index.html"));
-});
 
 const PORT = process.env.PORT || 3200;
 app.listen(PORT, () => {
