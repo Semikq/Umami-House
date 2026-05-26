7d926f23ee9c0ca5fe62f08244fd93d49314fc8b
Yaroslav <usern1861@gmail.com>
2026-05-26 23:35:58 +0300

    Update

--- server/server.ts
+++ server/server.ts
@@ -15,7 +15,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+        process.env.FRONTEND_URL || "",
+        process.env.VERCEL_URL || ""    
+    ],
     credentials: true,
 }));
 
